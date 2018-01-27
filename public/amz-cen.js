dea2f46ebbc3b52ee21977b6036e2325094fe7c8
clivemchd <clivemchd@gmail.com>
2018-01-27 13:24:28 +0530

    amz-cen: - adds about me section - adds contact section

--- public/amz-cen.js
+++ public/amz-cen.js
@@ -93,9 +93,9 @@ var about               = __webpack_require__(9)(compoPath + '/about/about.js');
 
 
 /*************************************************************************
- * baseModules to be injected in main module
+ * baseModules to be injected in main module amz-cen
  *************************************************************************/
-var baseModules =[
+var baseModules = [
   'ui.router',
   'duScroll',
    about.name
@@ -39818,7 +39818,11 @@ webpackContext.id = 9;
 module.exports = [
   '$scope'
   , function($scope) {
-  $scope.aboutMe = "This is clive";
+  $scope.aboutMe = { 
+    title : "About Me",
+    description: "I'm a Application Engineer with a year of experience based in Mumbai. I have a Master's Degree focused in Master of Computer Application from Thakur Institute of Management Studies, Career Development and research.", 
+    otherDescription: "So feel free to talk to me for all things tech & nerdy."
+  };
 }]
 
 /***/ })
--- public/main-mod/components/about/ctrl/about-mod-ctrl.js
+++ public/main-mod/components/about/ctrl/about-mod-ctrl.js
@@ -1,5 +1,9 @@
 module.exports = [
   '$scope'
   , function($scope) {
-  $scope.aboutMe = "This is clive";
+  $scope.aboutMe = { 
+    title : "About Me",
+    description: "I'm a Application Engineer with a year of experience based in Mumbai. I have a Master's Degree focused in Master of Computer Application from Thakur Institute of Management Studies, Career Development and research.", 
+    otherDescription: "So feel free to talk to me for all things tech & nerdy."
+  };
 }]
